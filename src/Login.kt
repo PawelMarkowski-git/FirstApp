999af0ec4fb7f40948473903ba47a0fd6474739a
Pawel Markowski <pawel.markowski88@gmail.com>
2019-11-03 15:12:44 +0100

    #9 Create new file Login.kt

--- src/Login.kt
+++ src/Login.kt
@@ -1,29 +1,52 @@
 fun login(){
 
-    val correctLogin: String = "pawel"
-    val correctPassword: String = "123"
+    println()
+    println("***************************************************************************************")
+    println("**************                    Logowanie                         *******************")
+    println("***************************************************************************************")
+
+
+    val correctLogin = arrayOf("pawel","pawel2","pawel13")
+    val correctPassword = arrayOf("123","321","pawel13")
 
     var login: String?
     var password: String?
 
-        do {
+
+    do {
+
+        var indexOfArrayCorrectLogin: Int = 0
+        var successLogin: Boolean = true
+        var issetLogin: Boolean = false
 
             println()
             print("Podaj login: ")
-            login = readLine()!!
+            login = readLine()
 
             print("Podaj hasło: ")
-            password = readLine()!!
+            password = readLine()
 
-            if(login != correctLogin || password != correctPassword){
 
-                println()
-                println("Błędny login lub hasło")
+            for (elementInArray in correctLogin) {
+
+                if (login == elementInArray) issetLogin = true
+
+                else if(login != elementInArray && !issetLogin) indexOfArrayCorrectLogin += 1
 
             }
 
-        }while(login != correctLogin || password != correctPassword)
+            when {
 
-    menuChoice()
+                indexOfArrayCorrectLogin == correctLogin.size -> {println("Taki użytkownik nie istnieje")
+                                                                  successLogin = false}
+
+                password != correctPassword[indexOfArrayCorrectLogin] -> {println("Błędne hasło")
+                                                                          successLogin = false}
+
+                password == correctPassword[indexOfArrayCorrectLogin] -> menuChoice()
+
+            }
+
+    }while(!successLogin)
 
 }
